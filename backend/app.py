ab87cb7bb15fc3a6014024ffcd35928834691f9b
Utkarsh Agrawal <utkarshagrawal016@gmail.com>
2026-04-22 07:06:05 +0530

    Added backend files

---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+import pickle
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="House Price Prediction API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = pickle.load(open("model.pkl", "rb"))
+
+class HouseInput(BaseModel):
+    Area: float
+    Bedrooms: int
+    Bathrooms: int
+    Age: int
+    Location: str
+
+@app.get("/")
+def home():
+    return {"message": "API is running successfully"}
+
+@app.post("/predict")
+def predict_price(data: HouseInput):
+    print("Location selected:", data.Location)
+
+    input_data = np.array([[data.Area, data.Bedrooms, data.Bathrooms, data.Age]])
+    prediction = model.predict(input_data)
+
+    # Simple city-based adjustment
+    location_multiplier = 1.0
+
+    if data.Location in ["Mumbai", "Delhi", "Bangalore"]:
+        location_multiplier = 1.15  # +15% for major cities
+    elif data.Location in ["Noida", "Pune", "Chandigarh"]:
+        location_multiplier = 1.08  # +8%
+
+    final_price = prediction[0] * location_multiplier
+
+    return {"predicted_price": round(final_price, 2)}
