be17b18dd10ec1885f635a9daba8fe8e1316a03b
Utkarsh Agrawal <utkarshagrawal016@gmail.com>
2026-04-25 10:02:07 +0530

    fix API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const predictPrice = async (data) => {
   const response = await axios.post(
-    "https://ml-tiny-project.onrender.com/",
+    "https://ml-tiny-project.onrender.com/predict",
     data
   );
   return response.data;
